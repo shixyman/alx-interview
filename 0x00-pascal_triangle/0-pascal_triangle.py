b21eed956bc93452af1a5601ad28aa9e80815d38
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-09 00:28:51 +0000

    testing

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,10 +10,10 @@ def pascal_triangle(n):
     for i in range(n):
         for j in range(n-i+1):
  
-            print(end=" ")
+            print(end="")
  
         for j in range(i+1):
  
-            print(factorial(i)/(factorial(j)*factorial(i-j)), end="")
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end="")
  
         print()

b29ec2686f036a46e5a88436f2eebd00ed6e3944
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-10 11:08:32 +0000

    modify

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,8 +2,6 @@
 '''
 Returns list representing the Pascal's triangle of n
 '''
-
-
 def pascal_triangle(n):
     '''returns empty list if n <= 0'''
     if n <= 0:
@@ -18,21 +16,3 @@ def pascal_triangle(n):
         bef_row = row
         triangle += [row]
     return triangle[1:]
-
-
-# #!/usr/bin/python3
-# def factorial(m):
-#     if m == 0:
-#         return 1
-#     else:
-#         return m * factorial(m - 1)
-    
-# def pascal_triangle(n):
-#     for i in range(n):
-#         row = []
-#         for j in range(n-i+1):
-#             print(end="")
-#         for j in range(i+1):
-#             coefficient = factorial(i)//(factorial(j)*factorial(i-j))
-#             row.append(coefficient)
-#         print(row)

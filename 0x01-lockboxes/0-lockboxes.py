7ca01395f3a433b9c14b5513c7d55a869311c968
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-16 13:15:13 +0100

    testing

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,34 @@
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    queue = [0]
-
-    while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if 0 <= key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-
-    return all(visited)
+    # Initialize a set to keep track of unlocked boxes
+    unlocked_boxes = set()
+    
+    # Function to perform DFS on the graph of boxes and keys
+    def dfs(box):
+        # If the box is already unlocked, return True
+        if box in unlocked_boxes:
+            return True
+        
+        # Mark the current box as unlocked
+        unlocked_boxes.add(box)
+        
+        # Try to unlock all boxes that can be unlocked from the current box
+        for key in boxes[box]:
+            if not dfs(key):
+                return False
+        
+        return True
+    
+    # Start DFS from the first box
+    return dfs(0)
 
 # Example usage
-boxes = [[1], [2], [3], []]
-print(canUnlockAll(boxes))  # Output: True
+boxes = [
+    [1, 2], # Box 0 can unlock boxes 1 and 2
+    [2],     # Box 1 can unlock box 2
+    []       # Box 2 has no keys
+]
+
+
 
 # def canUnlockAll(boxes):
 #     num_boxes = len(boxes)

3e2a884bde086c38cff9b347b7d93eaf142e9347
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-18 02:06:56 +0100

    modify

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,35 +1,21 @@
+#!/usr/bin/python3
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
+
+
 def canUnlockAll(boxes):
-  """
-  :type boxes: List[List[int]]
-  :rtype: bool
-  """
-  visited = set([0])
-  queue = [0]
-  while queue:
-    box = queue.pop(0)
-    for key in boxes[box]:
-      if key not in visited:
-        visited.add(key)
-        queue.append(key)
-  return len(visited) == len(boxes)
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
 
-
-
-
-
-
-# def canUnlockAll(boxes):
-#     num_boxes = len(boxes)
-#     visited = [False] * num_boxes
-#     visited[0] = True
-#     stack = [0]
-
-#     while stack:
-#         current_box = stack.pop()
-
-#         for key in boxes[current_box]:
-#             if key < num_boxes and not visited[key]:
-#                 visited[key] = True
-#                 stack.append(key)
-
-#     return all(visited)
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False

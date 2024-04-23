2ad99ddafb986b82487b784d63cff946917bb450
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-23 13:48:30 +0100

    modify

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -22,18 +22,10 @@ needed to result in exactly n H characters in the file.
 #     return operations[n] if operations[n] != float('inf') else 0
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     if n == 1:
         return 0
-
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        if n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        else:
-            divisor += 1
-
-    return operations
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n

09c256a7e52ed5d4dbefea8faaf39082b22b7fee
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-25 15:29:04 +0100

    modify

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,27 +5,6 @@ needed to result in exactly n H characters in the file.
 """
 
 
-# def minOperations(n: int) -> int:
-#     if n == 1:
-#         return 0
-
-#     operations = [float('inf')] * (n + 1)
-#     operations[1] = 0
-
-#     for i in range(2, n + 1):
-#         if n % i == 0:
-#             operations[i] = i
-#             for j in range(2, int(i ** 0.5) + 1):
-#                 if i % j == 0:
-#                     operations[i] = min(operations[i], operations[j] + i // j)
-
-#     return operations[n] if operations[n] != float('inf') else 0
-
-
-#!/usr/bin/python3
-'''Minimum Operations python3 challenge'''
-
-
 def minOperations(n):
     '''calculates the fewest number of
     operations needed to result in exactly n H

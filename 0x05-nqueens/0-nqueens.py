0ea23e638faafc3415375f4742f185ee36cdd35c
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-13 15:15:59 +0100

    test 2

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,45 +5,51 @@ A program that solves the N queens problem
 """
 import sys
 
-
 def solve_nqueens(n):
-    def is_safe(board, row, col):
-        # Check if a queen can be placed at the given position
-        for i in range(row):
-            if board[i] == col or \
-                board[i] + i == col + row or \
-                    board[i] - i == col - row:
-                return False
-        return True
-
-    def place_queens(board, row):
-        if row == n:
-            # All queens are placed, print the solution
-            print(board)
-            return
-
-        for col in range(n):
-            if is_safe(board, row, col):
-                board[row] = col
-                place_queens(board, row + 1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [-1] * n
-    place_queens(board, 0)
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    solutions = []
 
+    def is_safe(row, col):
+        # Check if a queen can be placed at the given position without attacking any other queen
+        for i in range(row):
+            if board[i][col] == 'Q':
+                return False
+            if col - (row - i) >= 0 and board[i][col - (row - i)] == 'Q':
+                return False
+            if col + (row - i) < n and board[i][col + (row - i)] == 'Q':
+                return False
+        return True
 
-# Check the command-line arguments
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    def backtrack(row):
+        if row == n:
+            # Found a valid solution, add it to the list of solutions
+            solutions.append([''.join(row) for row in board])
+            return
+        for col in range(n):
+            if is_safe(row, col):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    backtrack(0)
 
-solve_nqueens(N)
+    for solution in solutions:
+        for row in solution:
+            print(row)
+        print()
+
+if __name__ == '__main__':
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    solve_nqueens(n)

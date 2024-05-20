c475fdb15fc1571e12b31d36a97cd5d0c1a7a35f
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-20 17:09:15 +0100

    modify 4

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,29 +5,28 @@ const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   const movieId = process.argv[2];
-  const filmURL = `${API_URL}/films/${movieId}/`;
+  const filmUrl = `${API_URL}/films/${movieId}/`;
 
-  request(filmURL, (err, _, body) => {
+  request(filmUrl, (err, _, body) => {
     if (err) {
       console.log(err);
       process.exit(1);
     }
 
-    const charactersURL = JSON.parse(body).characters;
+    const charactersUrls = JSON.parse(body).characters;
     const charactersNames = [];
 
-    charactersURL.forEach((characterURL) => {
-      request(characterURL, (promiseErr, __, charactersReqBody) => {
-        if (promiseErr) {
-          console.log(promiseErr);
+    charactersUrls.forEach((characterUrl) => {
+      request(characterUrl, (error, __, charactersReqBody) => {
+        if (error) {
+          console.log(`Error fetching character data: ${error}`);
           process.exit(1);
         }
 
-        const characterData = JSON.parse(charactersReqBody);
-        const characterName = characterData.name;
+        const characterName = JSON.parse(charactersReqBody).name;
         charactersNames.push(characterName);
 
-        if (charactersNames.length === charactersURL.length) {
+        if (charactersNames.length === charactersUrls.length) {
           console.log(charactersNames.join('\n'));
         }
       });

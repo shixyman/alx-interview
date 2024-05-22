86c69c6316c4e855a159f09cbb960253af756d4b
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-22 22:25:48 +0100

    modify

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const API_URL = 'https://swapi-api.hbtn.io/api';
-
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  const filmUrl = `${API_URL}/films/${movieId}/`;
-
-  request(filmUrl, (err, _, body) => {
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
     if (err) {
-      console.log(err);
-      process.exit(1);
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
-
-    const charactersUrls = JSON.parse(body).characters;
-    const charactersNames = [];
-
-    charactersUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, __, charactersReqBody) => {
-        if (error) {
-          console.log(`Error fetching character data: ${error}`);
-          process.exit(1);
-        }
-
-        const characterName = JSON.parse(charactersReqBody).name;
-        charactersNames.push(characterName);
-
-        if (charactersNames.length === charactersUrls.length) {
-          console.log(charactersNames.join('\n'));
-        }
-      });
-    });
   });
-}
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);

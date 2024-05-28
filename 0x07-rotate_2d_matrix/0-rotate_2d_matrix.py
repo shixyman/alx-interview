3562398c4ebaa9965a32f95d785cdfb79638cece
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-28 16:07:53 +0100

    test4

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,12 +6,15 @@ matrix (list of lists): The input 2D matrix to be rotated.
 Returns:
 None
 """
+    n = len(matrix)  # Get the size of the matrix (n x n)
+    
     # Step 1: Transpose the matrix
-    for i in range(len(matrix)):
-        for j in range(i, len(matrix[i])):
-            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    for i in range(n):
+        for j in range(i, n):
+            # Swap elements at (i, j) and (j, i)
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     
     # Step 2: Reverse each row
-    for i in range(len(matrix)):
+    for i in range(n):
+        # Reverse the current row
         matrix[i] = matrix[i][::-1]
-
